1f1f2757265e10233d6e36959e18b85bae56a84f
Andre <andrey.bone2@gmail.com>
2025-07-17 19:00:44 +0200

    Add minimal bot with /start command using aiogram 3.4.1

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ async def start_handler(event: types.Message):
     await event.answer("Привет! Бот работает.")
 
 async def main():
-    bot = Bot(token=8051188469:AAGAv6h_jZ_d4TzKiOI3DgwoGiPCus4err4)
+    bot = Bot(token="8051188469:AAGAv6h_jZ_d4TzKiOI3DgwoGiPCus4err4")
     dp = Dispatcher()
 
     dp.message.register(start_handler, Command(commands=["start"]))
