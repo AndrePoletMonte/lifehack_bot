e3cfb844539bcfa911500f4dbe2883967e2168fe
Andre <andrey.bone2@gmail.com>
2025-07-18 23:46:51 +0200

    Add init_lifehacks.sql for DB setup

--- bot.py
+++ bot.py
@@ -1,17 +1,36 @@
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import BotCommand
+from dotenv import load_dotenv
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+import os
 
-async def start_handler(event: types.Message):
-    await event.answer("Привет! Бот работает.")
+from handlers import user_handlers
+from database import db
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+dp = Dispatcher(storage=MemoryStorage())
+
+async def set_default_commands(bot: Bot):
+    return await bot.set_my_commands([
+        BotCommand(command="start", description="Start bot"),
+        BotCommand(command="help", description="Help and info")
+    ])
+
+async def on_startup():
+    await db.init_db()
+    await set_default_commands(bot)
+
+dp.include_router(user_handlers.router)
 
 async def main():
-    bot = Bot(token="8051188469:AAGAv6h_jZ_d4TzKiOI3DgwoGiPCus4err4")
-    dp = Dispatcher()
-
-    dp.message.register(start_handler, Command(commands=["start"]))
-
-    print("Бот запущен. Ожидаю сообщений...")
+    await on_startup()
+    await bot.delete_webhook(drop_pending_updates=True)  # <- Удаляем webhook здесь
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
