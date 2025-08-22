6465e6d42869379704be895ec1aab8e46e69f132
AndrePoletMonte <andrey.bone2@gmail.com>
2025-08-22 10:24:12 +0200

    Update menu.py

--- keyboards/menu.py
+++ keyboards/menu.py
@@ -1,1 +1,7 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+def language_kb():
+    kb = InlineKeyboardMarkup()
+    kb.add(InlineKeyboardButton(text="English", callback_data="lang_en"))
+    kb.add(InlineKeyboardButton(text="Serbian", callback_data="lang_sr"))
+    return kb
