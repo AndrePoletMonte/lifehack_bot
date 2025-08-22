008b4797781e2ad5d1b8a1a237d8bfc3a18d81d9
AndrePoletMonte <andrey.bone2@gmail.com>
2025-08-22 17:36:55 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,14 +1,25 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import CommandStart
+
 from keyboards.menu import language_kb
-from database.db import add_user
+from database.database import add_user, update_user_language  # правильный импорт
+from data.texts import texts
 
 router = Router()
 
-@router.message(F.text == "/start")
-async def start_handler(message: Message):
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
     await add_user(message.from_user.id)
     await message.answer(
-        "Welcome! Please choose your language / Molim te izaberi jezik:",
-        reply_markup=language_keyboard()
+        texts["choose_language"]["en"],
+        reply_markup=language_kb()
     )
+
+
+@router.message(lambda message: message.text in ["English", "Serbian"])
+async def save_language(message: Message):
+    lang = "en" if message.text == "English" else "sr"
+    await update_user_language(message.from_user.id, lang)
+    await message.answer(texts["language_saved"][lang])
